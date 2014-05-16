fc1bb2eafaf6506685bb38797bad0641147bc060
kastendl86 <kastendl@gmail.com>
2014-05-16 16:16:33 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cyfarwydd::Application.config.secret_key_base = '440bfcada2af1b53732070bfbe4764684055d5917d598a55c26f4cffe066b05cdfb5a97998a5e1597bd7d8d83696bc309ee16920f26fd0d0e24a6ec70a0fb175'
